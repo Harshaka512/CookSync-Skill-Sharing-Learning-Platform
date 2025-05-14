e3a31be499e42fbeb5178e661491e3c1fbba3a56
Harshaka Herath <herathharshaka@gmail.com>
2025-05-14 23:39:29 +0530

    Comment.java
    Update Comment.java

--- skillsync/src/main/java/com/skillsync/cooking_edition/model/Comment.java
+++ skillsync/src/main/java/com/skillsync/cooking_edition/model/Comment.java
@@ -3,6 +3,8 @@ package com.skillsync.cooking_edition.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "comments")
 public class Comment {
@@ -15,6 +17,14 @@ public class Comment {
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private String parentCommentId;
+    private List<String> replies;
+    private int replyCount;
+
+    public Comment() {
+        this.replies = new ArrayList<>();
+        this.replyCount = 0;
+    }
 
     public String getId() {
         return id;
@@ -78,5 +88,44 @@ public class Comment {
 
     public void setUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    public String getParentCommentId() {
+        return parentCommentId;
+    }
+
+    public void setParentCommentId(String parentCommentId) {
+        this.parentCommentId = parentCommentId;
+    }
+
+    public List<String> getReplies() {
+        return replies;
+    }
+
+    public void setReplies(List<String> replies) {
+        this.replies = replies;
+    }
+
+    public int getReplyCount() {
+        return replyCount;
+    }
+
+    public void setReplyCount(int replyCount) {
+        this.replyCount = replyCount;
+    }
+
+    public void addReply(String replyId) {
+        if (this.replies == null) {
+            this.replies = new ArrayList<>();
+        }
+        this.replies.add(replyId);
+        this.replyCount++;
+    }
+
+    public void removeReply(String replyId) {
+        if (this.replies != null) {
+            this.replies.remove(replyId);
+            this.replyCount--;
+        }
     }
 }
