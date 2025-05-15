b76c2394d58c423b7023b9384e76007d114ffffc
Manoj Priyashantha <manojdulanjana7678@gmail.com>
2025-05-15 22:35:00 +0530

    Update App.js

--- skillsync/frontend/src/App.js
+++ skillsync/frontend/src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Community from './pages/Community';
+import MyProfile from './pages/MyProfile';
+import UserProfile from './pages/UserProfile';
+import PostList from './pages/posts/PostList';
+import PostView from './pages/posts/PostView';
+import PostCreate from './pages/posts/PostCreate';
+import PostEdit from './pages/posts/PostEdit';
+import LearningPlans from './pages/learning/LearningPlans';
+import LearningPlanDetail from './pages/learning/LearningPlanDetail';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-  <h1> hello</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="community" element={<Community />} />
+        <Route path="profile" element={<MyProfile />} />
+        <Route path="users/:userId" element={<UserProfile />} />
+        <Route path="posts" element={<PostList />} />
+        <Route path="posts/:id" element={<PostView />} />
+        <Route path="posts/create" element={<PostCreate />} />
+        <Route path="posts/:id/edit" element={<PostEdit />} />
+        <Route path="learning-plans" element={<LearningPlans />} />
+        <Route path="learning-plans/:id" element={<LearningPlanDetail />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
   );
 }
 
-export default App;
+export default App; 
